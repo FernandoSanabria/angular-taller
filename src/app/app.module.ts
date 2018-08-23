7c29407884bd033ceec690b3e41f992403a7c4ff
fmorenopinzon <fmorenopinzon@gmail.com>
2018-08-23 11:59:56 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 
 import { AppComponent } from './app.component';
+import { GirlsComponent } from './girls/girls.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GirlsComponent
   ],
   imports: [
-    BrowserModule
-  ],
+    BrowserModule,
+    FormsModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
